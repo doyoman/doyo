d7662397c85cd03f045d7bcf631475896a2af63b
doyoman <63859866+doyoman@users.noreply.github.com>
2021-07-13 23:49:21 +0800

    Add files via upload

---
+++ checkin.py
@@ -0,0 +1,48 @@
+from requests import session
+import requests
+import random
+import os
+
+########################
+bot_token = os.getenv("EU_BOT")
+chat_id = os.getenv("EU_ID")
+username = os.getenv("EU_USERNAME")
+password = os.getenv("EU_PASSWORD")
+########################
+
+def checkin():
+    sess = session()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    login_url = "https://j05.space/signin?c=" + str(random.random())
+
+    data = {
+        "email":username,
+        "passwd":password
+    }
+    #登录
+    sess.post(url=login_url, headers=headers, data=data)
+    #签到
+    checkin_url = "https://j05.space/user/checkin?c=" + str(random.random())
+    checkin_response = sess.post(url=checkin_url,headers=headers).json()['msg']
+
+    print(checkin_response)
+    send_messages("<b>几鸡签到:</b>\n" + checkin_response)
+
+def send_messages(text):
+    #telegram推送
+    bot_data = {
+                'chat_id': chat_id,
+                'text': text,
+                'parse_mode': 'html'
+    }
+    url = "https://api.telegram.org/bot" + bot_token + "/sendMessage"
+    rep = requests.post(url=url, data=bot_data)
+    if rep.status_code != 200:
+        print('telegram 推送失败')
+    else:
+        print('telegram 推送成功')
+
+if __name__ == '__main__':
+    checkin()
---
+++ teacher.py
@@ -0,0 +1,45 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import os
+import telegram
+
+provinces = ["hn","gd"]
+TOKEN = os.getenv("EU_BOT")
+ID = os.getenv("EU_ID")
+bot = telegram.Bot(token=TOKEN)
+
+for province in provinces:
+    if province == "hn":
+        citys = ["zz", "cs"]
+    elif province == "gd":
+        citys = ["gz",]
+
+    for city in citys:
+        url = "http://" + province + ".zgjsks.com/html/jiaozhao/ksgg/" + city
+
+        headers={
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36',
+        }
+
+        rep = requests.get(url=url,headers=headers)
+        rep.encoding='gbk'
+        html = rep.text
+        bs = BeautifulSoup(html,'html.parser')
+        text = bs.find_all("div",class_="c_list")
+        find = re.compile(r'.*</a><a href="(.*)" target="_blank" title=".*">(.*)</a></span><i>(.*)</i></li>')
+        gonggao_list = re.findall(find,str(text))
+        messages = []
+        for item in gonggao_list[0:6]:
+            message = "\n".join(item)
+            messages.append(message)
+        bot.send_message(chat_id=ID,text="\n----------\n".join(messages))
+        if city == "zz":
+            print("株洲信息发送完成！")
+        elif city == "cs":
+            print("长沙信息发送完成！")
+        elif city == "gz":
+            print("广州信息发送完成！")
+
+
+# print(text)
