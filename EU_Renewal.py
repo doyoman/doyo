d280cd0f6e0a108f02e907422d60ee90680ab3d5
doyoman <63859866+doyoman@users.noreply.github.com>
2021-07-14 01:15:52 +0800

    Update EU_Renewal.py

--- EU_Renewal.py
+++ EU_Renewal.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 """
 为了适配青龙识别定时：
-cron "20 7 15/24 * *"
+cron "20 7 15/24 * *" script-path=https://github.com/doyoman/doyo/raw/main/EU_Renewal.py, tag=EU续费
 new Env('EU续费')
 """
 
