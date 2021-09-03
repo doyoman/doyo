3e468169d359c0b61c38bcd71fb573de36814b49
doyoman <wowdoyo@gmail.com>
2021-09-03 03:35:36 +0000

    update

--- send_message.py
+++ send_message.py
@@ -3,19 +3,31 @@ import os
 
 #用于仓库内脚本发送通知
 
-chat_id = os.getenv("EU_ID")
-bot_token = os.getenv("EU_BOT")
+########获取环境变量##########
+
+CHAT_ID = os.getenv("EU_ID")
+BOT_TOKEN = os.getenv("EU_BOT")
+BARK_KEY = os.getenv("BARK_PUSH")
+
+##############################
 
 def send_message(text):
-    #telegram推送
-    bot_data = {
-        'chat_id': chat_id,
-        'text': text,
-        'parse_mode': 'html'
-    }
-    url = "https://api.telegram.org/bot" + bot_token + "/sendMessage"
-    rep = requests.post(url=url, data=bot_data)
-    if rep.status_code != 200:
-        print('telegram 推送失败')
-    else:
-        print('telegram 推送成功')
+        if len(BOT_TOKEN) == 0:    #telegram推送
+        bot_data = {
+            'chat_id': CHAT_ID,
+            'text': text,
+            'parse_mode': 'html'
+        }
+        url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
+        rep = requests.post(url=url, data=bot_data)
+        if rep.status_code != 200:
+            print('telegram 推送失败')
+        else:
+            print('telegram 推送成功')
+    elif len(BARK_KEY) == 0:    #bark推送
+        rep = requests.post('https://api.day.app/' + BARK_KEY + '/' + text)
+        if rep.status_code != 200:
+            print('Bark 推送失败')
+        else:
+            print('Bark 推送成功')
+
