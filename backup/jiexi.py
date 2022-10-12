6a73365748d374261002de2c5a6ac9cc2536ba8a
doyoman <63859866+doyoman@users.noreply.github.com>
2022-10-12 16:20:03 +0800

    Update jiexi.py

--- backup/jiexi.py
+++ backup/jiexi.py
@@ -23,15 +23,15 @@ def download(d_url, file_name):
 
 
 if __name__ == "__main__":
-    # link = input("请输入链接：")
-    link = sys.argv[1]
+    link = input("请输入链接：")
+    #link = sys.argv[1]
     global headers
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
     }
-    file_name = link.split("/")[-1]
+    rsp = jiexi(link)
+    file_name = rsp["url"].split("/")[-1]
     if file_name =="":
         file_name = link.split("/")[-2]
-    rsp = jiexi(link)
     print(rsp["video"])
     download(rsp["video"], file_name)
